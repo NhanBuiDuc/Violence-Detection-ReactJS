81899a4a6045dae0691678c972703366e9283561
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2022-12-13 22:47:47 +0700

    update subcription

--- src/model/Subcription.js
+++ src/model/Subcription.js
@@ -21,4 +21,25 @@ export default class Subcription {
             console.log("In CreateSubcription, resonse = ", response)
             return response
     }
+    static async getSubcriptionByAccountId(account_id){
+        let action = '/subcriptions/account_id/'
+        let myURL = baseURL + action
+
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        const requestBody = {account_id, service_id}
+        let response = await fetch(myURL, {
+            mode: 'cors',
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(requestBody)
+        }).then(function(response){
+            return response.json();
+        }).then(function(myJson) {
+            return myJson
+        });
+        console.log("In CreateSubcription, resonse = ", response)
+        return response
+}
 }
