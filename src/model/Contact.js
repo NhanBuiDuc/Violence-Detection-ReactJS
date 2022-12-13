8953aba266e2729a9538f11dd75a818d5484e204
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2022-12-13 21:03:40 +0700

    UPDATE contact delete

--- src/model/Contact.js
+++ src/model/Contact.js
@@ -131,4 +131,30 @@ export default class Contact {
             return null
         }
     }
+    static delete = async (contact_id) => {
+        try{
+            let action = '/contacts/delete/contact_id'
+            let myURL = baseURL + action
+            let headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+            const requestBody = {contact_id}
+            let response = await fetch(myURL, {
+                mode: 'cors',
+                method: 'DELETE',
+                headers: headers,
+                body: JSON.stringify(requestBody)
+            }).then(function(response){
+                return response.json();
+            }).then(function(myJson) {
+                return myJson
+            });
+            console.log("In Contact, delete's resonse = ", response)
+            
+            return response
+        }
+        catch(err){
+
+            return null
+        }
+    }
 }
