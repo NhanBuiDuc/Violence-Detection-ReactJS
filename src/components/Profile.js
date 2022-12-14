f5e4a23db3b4e981664bd00a4ab53bb7f0b0b45d
Kharis <nguyentrankhai123@gmail.com>
2022-12-15 00:02:54 +0700

    update

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,7 +7,6 @@ import CurrentUser from "../model/CurrentUser";
 import Account from "../model/Account";
 import axios, { Axios } from "axios";
 import { ConstructionOutlined } from "@mui/icons-material";
-import Demo from "./CameraCard";
 // var baseURL = 'https://localhost:8000/'
 var baseURL = 'https://c9b80c4b-4436-4358-8ab8-2bc97afbc640.mock.pstmn.io'
 
@@ -79,7 +78,6 @@ export function UserProfile () {
                             {/* <form className="subScription">
                                 <p>Hell1o</p>
                             </form> */}
-                            <Demo></Demo>
                         </div>
                         
                 </section>
@@ -108,7 +106,6 @@ export function UserProfile () {
                 {/* <form className="subScription">
                                 <p>Hello1</p>
                             </form> */}
-                <Demo></Demo>
             </div>
     </section>
 </>
