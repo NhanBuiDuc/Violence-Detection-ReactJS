458e4d3d78bfd9c630827c9b92d6eaca2e8f683a
VietLong0601 <levietlongx2@gmail.com>
2022-12-23 23:21:08 +0700

    message

--- src/components/pages/Streaming.jsx
+++ src/components/pages/Streaming.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-// import './css/streaming.css'
+import '../../App.css';
+
 import './css/streaming.css'
 import CardItem from '../CardItem'
 import logo from "../videos/video-1.mp4"
+import logo2 from "../videos/video-2.mp4"
 function Streaming() {
     return (
-        <div className="App">
+        <div className="stream-content">
            <div className="App-header">
             <video src={logo} className="App-logo" alt="logo" autoPlay loop muted />
-            <img src={'https://e4da-171-247-145-94.ap.ngrok.io/video_feed'} className="App-logo" alt="logo" />
+            <video src={logo2} className="App-logo2" alt="logo2" autoPlay loop muted />
+            {/* <img src={'https://e4da-171-247-145-94.ap.ngrok.io/video_feed'} className="App-logo" alt="logo" />
             <img src={'/video_feed'} className="App-logo" alt="logo" />
-            <img src={'/video_feed'} className="App-logo" alt="logo" />
-          
-          </div> */
-          
-          
+            <img src={'/video_feed'} className="App-logo" alt="logo" /> */}
+            </div> 
+            <div classname="App-bottom">
+            
+            </div>
         </div>
+
       );
   }
   
